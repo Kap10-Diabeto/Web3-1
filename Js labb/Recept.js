4d045b5de6c7b99c869c0aa886d91383dc44619f
Kap10-Diabeto <maps00001@utb.vaxjo.se>
2019-09-05 10:18:55 +0200

    nya uppgifter

---
+++ Js labb/Recept.js
@@ -0,0 +1,31 @@
+let recept = {
+    title: "Brunbärspasta",
+    rating: "5.5/5",
+    ingredients: [
+        "200g Brunbär, ","1ml Muskottnöt, ","400g kylpasta"
+    ],
+
+    steps: [
+        "Gnugga bären, ", "Steka kylpastan, ", "Smula muskottnöten, ", "Blanda, ", "Servera"
+    ]
+}
+//Tomt värde i print, functionen fyller på med varje index i arrayen
+var print = "";
+recept.ingredients.forEach(myFunction);
+
+function myFunction(value){
+print = print + value; 
+return print;
+}
+
+console.log(print);
+
+var print = "";
+recept.steps.forEach(myFunction);
+
+function myFunction(value){
+print = print + value; 
+return print;
+}
+
+console.log(print);
